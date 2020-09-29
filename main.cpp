a01342b8b27a23c17b81ec85bda33b4ada2be746
Rafal Cardona <rafael.cardona@alumnos.udg.mx>
2020-09-28 19:52:20 -0500

    Uso de getline para leer cadena incluyendo espacios e impresion de tamanio

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
     string cadena;
-    cin >> cadena;
-    cout << cadena << endl;
+    //cin >> cadena;
+    getline(cin, cadena);
+    cout << cadena.size() << endl;
 
     system("pause");
     return 0;
